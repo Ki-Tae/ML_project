403a177bcc2ea819c8f7764eeaa8a50f27d1d842
Dave Khim <kitae118294@gmail.com>
2018-11-15 13:21:03 +0900

    loading pickle data

---
+++ model/data.py
@@ -0,0 +1,14 @@
+import pickle
+
+# loading the data
+
+def load_dict(A_dict_file_path, X_dict_file_path)
+    with open(A_dict_file_path, 'rb') as f:
+        A_dict_subset = pickle.load(f)
+
+    with open(X_dict_file_path, 'rb') as f:
+        X_dict_subset = pickle.load(f)
+
+    return A_dict_subset, X_dict_subset
+
+
