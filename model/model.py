d6f23ef776ec7f813304c2cb53fab1f58f2bc7bd
Dave Khim <kitae118294@gmail.com>
2018-11-25 02:02:02 +0900

    data > normalised

--- model/model.py
+++ model/model.py
@@ -4,21 +4,22 @@ import keras
 from model_data import load_dict, convert_to_inputs_outputs
 from random import randint
 
-max_epoch = 20000
+max_epoch = 10000
 
 """
 Define input/output.
 """
 size_input = 29
 num_molecules = 1000
-AXE_file_path = "C:\KT_project\dataset\AXE_dict_subset\AXE_dict_subset0.pickle"
-AXE_dict_subset = load_dict(AXE_file_path = AXE_file_path)
-results = convert_to_inputs_outputs(AXE_dict_subset=AXE_dict_subset, molecule_num=num_molecules, subset_num=0)
+AXEN_file_path = "C:\KT_project\dataset\AXEN_dict_subset\AXEN_dict_subset0.pickle"
+AXEN_dict_subset = load_dict(AXEN_file_path = AXEN_file_path)
+results = convert_to_inputs_outputs(AXEN_dict_subset=AXEN_dict_subset, molecule_num=num_molecules, subset_num=0)
 A_array_list = results['A']
 X_array_list = results['X']
 output_E_list = results['E']
+atom_num_list = results['N']
 print("inputs and outputs are converted.")
-print(A_array_list[0].shape)
+
 
 """
 Define model.
@@ -33,13 +34,13 @@ outputs = tf.placeholder(tf.float32, [None, 1], name="Output_layer")
 print(outputs.shape)
 # 1st layer
 x = tf.matmul(A, inputs, name="op_1")
-x = tf.keras.layers.Dense(30, activation='relu', kernel_initializer=tf.initializers.he_normal(seed=1), name="layer1")(x)
+x = tf.keras.layers.Dense(50, activation='relu', kernel_initializer=tf.initializers.he_normal(seed=1), name="layer1")(x)
 # 2nd layer
 x = tf.matmul(A, x, name="op_2")
-x = tf.keras.layers.Dense(60, activation='relu', kernel_initializer=tf.initializers.he_normal(seed=1), name="layer2")(x)
+x = tf.keras.layers.Dense(100, activation='relu', kernel_initializer=tf.initializers.he_normal(seed=1), name="layer2")(x)
 # 3rd layer
 x = tf.matmul(A, x, name="op_3")
-x = tf.keras.layers.Dense(30, activation='relu', kernel_initializer=tf.initializers.he_normal(seed=1), name="layer3")(x)
+x = tf.keras.layers.Dense(50, activation='relu', kernel_initializer=tf.initializers.he_normal(seed=1), name="layer3")(x)
 predictions = tf.keras.layers.Dense(1, activation='sigmoid', kernel_initializer=tf.initializers.variance_scaling(scale=1.0), name="layer4")(x)
 print(predictions)
 
@@ -50,7 +51,8 @@ Thus, dim [size_input*num_molecules, 1] -> (reshape)
       dim [size_input, num_molecules, 1] -> (reduce_sum)
       dim [num_molecules, 1]
 """
-predictions = tf.reduce_sum(tf.reshape(predictions, [size_input, -1, 1]), axis=0, name="op_sum")
+predictions = tf.reduce_sum(tf.reshape(predictions, [size_input, -1, 1]), axis=0, name="op_row_sum")
+# predictions = tf.segment_sum(predictions, name="op_sum")
 print(predictions)
 
 """
@@ -58,21 +60,26 @@ After defining the neural network output,
 we use tensorflow to set loss function and train the model.
 """
 
-loss = tf.losses.mean_squared_error(labels = outputs, predictions = predictions)
-tf.summary.scalar('loss', loss)
+loss_adam = tf.losses.mean_squared_error(labels = outputs, predictions = predictions)
+# loss_rms = tf.losses.mean_squared_error(labels = outputs, predictions = predictions)
+# loss_grad = tf.losses.mean_squared_error(labels = outputs, predictions = predictions)
+
+tf.summary.scalar('loss_adam', loss_adam)
+# tf.summary.scalar('loss_rms', loss_rms)
+# tf.summary.scalar('loss_grad', loss_grad)
+
 with tf.name_scope("train"):
-    optim = tf.train.AdamOptimizer(learning_rate=0.00001)
-    #optim = tf.train.RMSPropOptimizer(learning_rate=0.01)
-    #optim = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-    minimize = optim.minimize(loss)
+    optim1 = tf.train.AdamOptimizer(learning_rate=0.0001)
 
+    minimize1 = optim1.minimize(loss_adam)
 
+    
 # To start tensorflow, we open the tensorflow session
 with tf.Session() as sess:
     # before training, we need to initialize the variables(=weights)
     sess.run(tf.global_variables_initializer())
     merged_summary=tf.summary.merge_all()
-    writer = tf.summary.FileWriter("C:\KT_project\gcn\model\hist\\6")
+    writer = tf.summary.FileWriter("C:\KT_project\gcn\model\hist\\after_normalisation")
     writer.add_graph(sess.graph)
     # code above writes the graph in tensorboard
     for epoch in range(max_epoch):
@@ -80,13 +87,22 @@ with tf.Session() as sess:
         A_matrix = A_array_list[ran_num]
         X_matrix = X_array_list[ran_num]
         E = output_E_list[ran_num]
-        minimize.run(feed_dict={inputs: X_matrix, A: A_matrix, outputs: E})
+        
+        minimize1.run(feed_dict={inputs: X_matrix, A: A_matrix, outputs: E})
+        
+        # 
+        if epoch == 0:
+            print(epoch+1, sess.run(loss_adam, feed_dict={inputs: X_matrix, A: A_matrix, outputs: E}))
+
         if (epoch+1) % 100 == 0:
             # show loss for every 100th epoch
-            # print(epoch+1, sess.run(loss, feed_dict={inputs:np.transpose(dummy_input), outputs:dummy_output.reshape([-1,1])}))
-            # print(epoch+1, sess.run(loss, feed_dict={inputs: X_matrix, A: A_matrix, outputs: E}))
+            # print(epoch+1, sess.run(loss_adam, feed_dict={inputs: X_matrix, A: A_matrix, outputs: E}))
             s = sess.run(merged_summary, feed_dict={inputs: X_matrix, A: A_matrix, outputs: E})
             writer.add_summary(s, epoch+1)
             # code above writes summary of the session and writes to the disk.
 
-# model 학습과정 표시하기
+# A input 변경
+# node 수 변경
+# optimizer 변경
+# epoch
+# input 
--- model/model_data.py
+++ model/model_data.py
@@ -3,27 +3,39 @@ import pickle
 
 # loading the data
 """
-define how many molecules to put for 1 batch
-molecule_num = 10
-molecule_num = 10
-AXE_file_path = "C:\KT_project\dataset\AXE_dict_subset\AXE_dict_subset0.pickle"
+each X are normalized differently.
 """
+
 max_X = 9**2.4/2*841
 Ha_to_ev = 1000
+"""
 
-def load_dict(AXE_file_path):
-    with open(AXE_file_path, 'rb') as f:
-        AXE_dict_subset = pickle.load(f)
+max U0 ['molecule0'] = -1101.4877900833399
+min U0 ['molecule133620'] = -19444.3873485546
 
-    return AXE_dict_subset
+"""
+minE = 1101.4877900833399
+maxE = 19444.3873485546
+maxX = 0.5*9**2.4
 
-def convert_to_inputs_outputs(AXE_dict_subset, molecule_num, subset_num):
+
+
+def load_dict(AXEN_file_path):
+    with open(AXEN_file_path, 'rb') as f:
+        AXEN_dict_subset = pickle.load(f)
+
+    return AXEN_dict_subset
+
+def convert_to_inputs_outputs(AXEN_dict_subset, molecule_num, subset_num):
     A_array_list = []
     X_array_list = []
     output_E_list = []
+    molecule_atom_num_list = []
+    
+
 
     for i in range(molecule_num):
-        A_hat = AXE_dict_subset['molecule{}'.format(subset_num*1000 + i)][0]
+        A_hat = AXEN_dict_subset['molecule{}'.format(subset_num*1000 + i)][0]
         D_hat = np.diag(np.sum(A_hat, axis=0))
         D_inv = np.linalg.inv(D_hat)
         A = np.dot(D_inv, A_hat)
@@ -34,28 +46,43 @@ def convert_to_inputs_outputs(AXE_dict_subset, molecule_num, subset_num):
         A_array = np.asarray(A_array)
         # A_array's shape is (1,29,29)
 
-        X = AXE_dict_subset['molecule{}'.format(subset_num*1000 + i)][1]/max_X
+        N = AXEN_dict_subset['molecule{}'.format(subset_num*1000 + i)][3]
+        N_array = [
+            N
+        ]
+        N_array = np.asarray(N).reshape([-1,1])
+        # N_array's shape is (1,1)
+
+        # X normalisation
+        X = AXEN_dict_subset['molecule{}'.format(subset_num*1000 + i)][1]
+        X = X/maxX/N**2
         X_array = [
             X
         ]
         X_array = np.asarray(X_array)
         # X_array's shape is (1,29,29)
 
-        E = AXE_dict_subset['molecule{}'.format(subset_num*1000 + i)][2]/Ha_to_ev
+        E = AXEN_dict_subset['molecule{}'.format(subset_num*1000 + i)][2]
+        E = (-E-minE)/(maxE-minE)
+        # E normalisation btw (0, 1)
         E_array = [
             E
         ]
         E_array = np.asarray(E_array).reshape([-1,1])
         # E_array's shape is (1,1)
 
+        
+
         A_array_list.append(A_array)
         X_array_list.append(X_array)
         output_E_list.append(E_array)
+        molecule_atom_num_list.append(N_array)
         
     result = {
         'A' : A_array_list,
         'X' : X_array_list,
-        'E' : output_E_list
+        'E' : output_E_list,
+        'N' : molecule_atom_num_list
     }
 
     return  result
